3b2cd7632d3567722ecd23e4abd7f982c674368c
Zyy955 <1072411694@qq.com>
2023-10-10 18:23:47 +0800

    fix：修复`getGroupMemberInfo`

--- plugins/icqq.js
+++ plugins/icqq.js
@@ -102,7 +102,7 @@ async function pickUser_msg(uin, group_id, content) {
 
 /** 劫持修改getGroupMemberInfo方法 */
 Bot.getGroupMemberInfo = async function (group_id, id) {
-    if (String(group_id).includes("qg_", "@")) {
+    if (/qg_|@/.test(String(group_id))) {
         const scene = String(group_id).includes("qg_") ? "QQGuild-Bot" : "WeChat-Bot"
         return {
             group_id: group_id,
